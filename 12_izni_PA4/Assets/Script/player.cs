b83098f5136a76eef60748c26170a88609c34842
izni-syaz <64114960+izni-syaz@users.noreply.github.com>
2020-04-30 14:10:32 +0800

    Added a unity project

---
+++ 12_izni_PA4/Assets/Script/player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public GameObject Sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Sphere.transform.position, new Vector3(0, 1, 0), 180 * Time.deltaTime);
+    }
+}
