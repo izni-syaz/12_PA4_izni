c2afb52d328b10a9ab00d63832e9b4d19ca67469
izni-syaz <64114960+izni-syaz@users.noreply.github.com>
2020-04-30 15:19:53 +0800

    Adding MainScene

---
+++ 12_izni_PA4/Assets/Script/scene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Wait");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("CubeScene");
+    }
+}
